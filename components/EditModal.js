0122e08659e947ad6a5bd7ce2ed0336eb36fdfa2
madzadev <madars.biss@gmail.com>
2022-04-25 19:42:06 +0200

    style the edit card

--- components/EditModal.js
+++ components/EditModal.js
@@ -1,4 +1,5 @@
 import { useCallback, useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 import Modal, {
   ModalBody,
@@ -7,6 +8,7 @@ import Modal, {
   ModalTitle,
   ModalTransition,
 } from "@atlaskit/modal-dialog";
+import styles from "./EditModal.module.css";
 
 export default function EditModal({ editModalVisible }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +20,25 @@ export default function EditModal({ editModalVisible }) {
       setIsOpen(!isOpen);
     };
   }, [editModalVisible]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { title, description, due_date } = data;
+    console.log(title);
+    // try {
+    //   const login = await api.login(email, password);
+    //   const { $id } = login;
+    //   setUser($id);
+    //   router.push("/boards");
+    // } catch (err) {
+    //   console.log(err.message);
+    // }
+  };
 
   return (
     <div>
@@ -34,17 +55,41 @@ export default function EditModal({ editModalVisible }) {
               Duplicating this page will make it a child page of{" "}
               <span>Search - user exploration</span>, in the{" "}
               <span>Search & Smarts</span> space.
-              <p>Title</p>
-              <input type="text" />
-              <p>Description</p>
-              <input type="text" />
+              <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+                <p>Enter the title</p>
+                <input
+                  {...register("title", { required: true })}
+                  className={styles.input}
+                />
+                {errors.title && <span>Enter a valid title</span>}
+                <input
+                  {...register("description", { required: true })}
+                  className={styles.input}
+                />
+                {errors.title && <span>Enter a valid description</span>}
+                <input
+                  {...register("due_date", { required: true })}
+                  className={styles.input}
+                />
+                {errors.due_date && <span>Enter a valid due date</span>}
+
+                {/* <input type="submit" className={styles.submit} /> */}
+                <ModalFooter>
+                  <button onClick={closeModal} className={styles.cancel}>
+                    Cancel
+                  </button>
+                  <button type="submit" className={styles.add} autoFocus>
+                    Add
+                  </button>
+                </ModalFooter>
+              </form>
             </ModalBody>
-            <ModalFooter>
+            {/* <ModalFooter>
               <button onClick={closeModal}>Cancel</button>
               <button onClick={closeModal} autoFocus>
                 Add
               </button>
-            </ModalFooter>
+            </ModalFooter> */}
           </Modal>
         )}
       </ModalTransition>
