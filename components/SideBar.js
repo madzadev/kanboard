ee16fc9a7be683926b03e70b85c3c9332f0a22f9
madzadev <madars.biss@gmail.com>
2022-04-27 22:31:34 +0200

    edit sidebar

--- components/SideBar.js
+++ components/SideBar.js
@@ -9,7 +9,11 @@ const SideBar = () => {
   return (
     <div className={styles.wrapper}>
       <AddBoardModal addBoardModalVisible={addBoardModalVisible} />
-      <h1 className={styles.title}>Trello servers</h1>
+      <h2 className={styles.title}>Dashboard</h2>
+      <h2 className={styles.title}>Notifications</h2>
+      <h2 className={styles.title}>Calendar</h2>
+      <hr />
+      <h2 className={styles.title}>Boards list</h2>
       <h3 className={styles.page}># Articles</h3>
       <h3 className={styles.page}># Goals</h3>
       <h3 className={styles.page}># Tech</h3>
