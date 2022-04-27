b2abf7c1c98cedb336c38e2155d3cc102f683cd8
madzadev <madars.biss@gmail.com>
2022-04-27 22:28:08 +0200

    add board modal

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
+import AddBoardModal from "./AddBoardModal";
 import styles from "./SideBar.module.css";
 
 const SideBar = () => {
+  const [addBoardModalVisible, setAddBoardModalVisible] = useState(false);
+
   return (
     <div className={styles.wrapper}>
+      <AddBoardModal addBoardModalVisible={addBoardModalVisible} />
       <h1 className={styles.title}>Trello servers</h1>
       <h3 className={styles.page}># Articles</h3>
       <h3 className={styles.page}># Goals</h3>
       <h3 className={styles.page}># Tech</h3>
       <h3 className={styles.page}># Books</h3>
       <h3 className={styles.page}># Movies</h3>
-      <h2>+ Add new</h2>
+      <div
+        className={styles.add}
+        onClick={() => {
+          setAddBoardModalVisible(!addBoardModalVisible);
+        }}
+      >
+        + New server
+      </div>
     </div>
   );
 };
