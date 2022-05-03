23397e162c2342d58afc994929393c020d942e51
madzadev <madars.biss@gmail.com>
2022-05-03 07:58:29 +0200

    edit dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,6 +4,8 @@ const Dashboard = () => {
   return (
     <AuthWrapper>
       <h1>This is the dashboard page</h1>
+      <h3>Total tasks: 13</h3>
+      <h3>Total users: 23</h3>
       <h3>Calendar</h3>
       <h3>Recent activity</h3>
     </AuthWrapper>
